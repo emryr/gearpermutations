dc89b2bf917fefde9bf76f4bcc8d41e55d3bfe3b
Plikt <desotaelianna@gmail.com>
2021-07-01 22:52:05 -0400

    Update 20210627_gearpermutations_finalcode.py

--- 20210627_gearpermutations_finalcode.py
+++ 20210627_gearpermutations_finalcode.py
@@ -2,68 +2,73 @@
 """
 Spyder Editor
 """
-import numpy as np
 import itertools
-import xlwt
-from xlwt import Workbook
-import math
+#from xlwt import Workbook
 from openpyxl import load_workbook
+import numpy as np
+import pandas as pd
 
-#name the workbook 
-wb = Workbook()
+#name the workbook z
+path = "C:\\Users\\Psych account\\"
+wb = load_workbook(path + '20210613_gearpermutation2.xlsx')
+
 
 #Add a sheet and name it. Allow cell overwrite
-permwb = wb.add_sheet('gearpermutation', cell_overwrite_ok=True)
+permwb = wb.create_sheet('gearpermutations', 0)
 
-#initiating my variables
+#initiating all variables
 
 #Gear names - can be changed to whatever you'd like to label the gears
-gearnames = ['compound type','Gear 1', 'Gear 2', 'Gear 3', 'Gear 4', 'Gear 5']
+column = ['compound type','Speed Ratio','Gear 1', 'Gear 2', 'Gear 3', 'Gear 4', 'Gear 5', 'Gear 6', 'Gear 7'] 
 
-values = [2, 4, 6, 8, 10] #array of gears w desired teeth numbers
+values = [2, 4, 6, 8, 10, 16, 24] #array of gears w desired teeth numbers
 
 per = [] #storage of the number of permultations given len(values) choose i
-per1 = [] #solution array
-speed = ['speed'] #the speed ratio of the gears
+sol = [] #solution array
+gearspeed = 1
+index = 0
+
+def genbin(n, bs=''):
+    if len(bs) == n:
+        binom.append(bs)
+    else:
+        genbin(n, bs + '0')
+        genbin(n, bs + '1')
 
 #For loop which stores all the permutations in which you can choose i of the length of the values array
-for choose in range(len(values)+1):
+for choose in range(1, len(values)+1):
+    binom = []
+
     #per is a forced list of a map which forces all the tuples in itertools to lists in python - a bit clunky, but functions
     per =  list(map(list, itertools.permutations(values, choose)))
+    genbin(choose-1)
+    binom = list(map(str, binom))
 
-    #checks if i > 1. i < 1 means that there are not two gears to compound
-    if choose > 1: 
-        #itereates over the number of required replications of possible compound gears 
-        for rep in range(choose):
-            #iterates over the length of the permutations given len(values) choose (choose) from above
-            for index in range(len(per)):               
-                per1.append(list(per[index])) #appends per at a given index to the solution array
-                per1[-1].insert(0,rep) #inserts the replication number to track which combo is happening - not elegant 
-                
-                #Appends the calculated speed to the speed array, checks if rep = 0
-                #checks if the replication is 0, because the calculation for no compunds is different than with compounds
-                if rep ==0:
-                    speed.append(per[index][rep]/per[index][-1])
-                else:
-                    speed.append(per[index][0]/per[index][rep-1] * per[index][rep]/per[index][-1])
-                
-    else: 
-        #iterates over the length of per in the case that there are no possible compound gears to be made
-        for length in range(len(per)): 
-            speed.append(1) #adds the only speed reduction possible with less than 2 gears
-            per1.append(per[length]) #appends a given permutation to the solution array
-            per1[-1].insert(0,0) #adds the 'combination' type
+    print(len(binom), binom)
+
+    #itereates over the number of required replications of possible compound gears 
+    for currentperm in range(len(per)):
+        #iterates over the length of the permutations given len(values) choose (choose) from above
+        for rep in range(len(binom)):               
+            sol.append(list(per[currentperm])) #appends per at a given index to the solution array
+            sol[-1].insert(0,binom[rep]) #inserts the replication number to track which combo is happening - not elegant 
+            gearspeed = 1
+   
+            #Appends the calculated speed to the speed array, checks if rep = 0
+            #checks if the replication is 0, because the calculation for no compunds is different than with compounds
+            for index in range(len(per[currentperm])-1):
+                #print(binom)
+                if binom[rep][index] == '0': 
+                    gearspeed = gearspeed*(per[currentperm][index]/per[currentperm][index+1])
+                                
+            sol[-1].insert(1, gearspeed)
 
 
-print(len(per1), len(speed))
-per1.insert(0, gearnames[0:len(gearnames)])
-#print(per1)
+print(len(sol))
 
-#iterates over the 2d array of the output and prints it to an excel workbook. 
-for i in range(len(per1)):
-    permwb.write(i, len(gearnames), speed[i])
-    for n in range(len(per1[i])):
-        permwb.write(i,n, per1[i][n])
+df = pd.DataFrame(sol, columns = column)
+sort = df.sort_values(by = ['Speed Ratio'])
+print(sort)
 
-#saves the workbook        
-wb.save('20210613_gearpermutation2.xls')
+sort.to_csv("C:/Users/Psych account/Documents/gearpermutations/20210107gearpermutations.csv")                  
+
